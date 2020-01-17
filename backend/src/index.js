319ba7e39b18c04296b0ba203a3326163596eb73
axell-brendow <breno.axel@gmail.com>
2020-01-17 00:59:34 -0300

    Implement all the front-end

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const mongose = require('mongoose');
+const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
 // Make express understand json format in all routes
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
 // Mongo DB Cluster
-mongose.connect('mongodb+srv://axell:Manage13@cluster0-owhuo.mongodb.net/week10?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://axell:Manage13@cluster0-owhuo.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
