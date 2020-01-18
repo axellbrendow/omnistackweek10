fca1cf27b4f18d2d29e5933e977952e5f0a96d2f
axell-brendow <breno.axel@gmail.com>
2020-01-18 18:11:56 -0300

    Start mobile project using React Native

---
+++ mobile/src/pages/Main.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
+
+function Main()
+{
+    useEffect(() => {
+        async function loadInitialPosition()
+        {
+            //
+        }
+
+        loadInitialPosition();
+    }, []);
+
+    return <MapView style={styles.map}/>;
+}
+
+const styles = StyleSheet.create({
+    map: {
+        flex: 1
+    },
+});
+
+export default Main;
