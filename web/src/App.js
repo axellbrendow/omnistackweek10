53507f8f5a3eae8078b809060d72e8c19ddb7add
axell-brendow <breno.axel@gmail.com>
2020-01-18 17:10:50 -0300

    Change Portuguese word to English

--- web/src/App.js
+++ web/src/App.js
@@ -31,7 +31,7 @@ function App() {
   return (
     <div id="app">
       <aside>
-        <strong>Cadastrar</strong>
+        <strong>Register</strong>
         <DevForm onSubmit={handleAddDev}/>
       </aside>
 
